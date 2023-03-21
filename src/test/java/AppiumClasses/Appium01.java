69d07b2c611ed1a8f427a3a06bdace4a5c9f1c89
Sebahattin Demir <info.sebahattindemir@gmail.com>
2023-03-21 17:52:59 +0300

    21/03/2023

--- src/test/java/AppiumClasses/Appium01.java
+++ src/test/java/AppiumClasses/Appium01.java
@@ -1,4 +1,34 @@
 package AppiumClasses;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Appium01 {
+
+    @Test
+    public void test() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+//  capabilities.setCapability("platformName","Android"); --> Bu şekilde de yazımı var
+//  capabilities.setCapability(CapabilityType.PLATFORM_NAME,"Android"); --> Bu şekilde de yazımı var
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice"); // Android de fark etmiyor böyle de yazılabilirveya cmd --> adb devices yazdığında gelen değeri de yazabilirsin
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\sdemi\\IdeaProjects\\mobileTesting\\src\\Apps\\Gesture Tool_1.3_Apkpure.apk");
+
+        capabilities.setCapability("appPackage","com.davemac327.gesture.tool");
+        capabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
+    }
 }

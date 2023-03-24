064ab0a4eb39a4016b6a824993bf9acf35aa6b77
Sebahattin Demir <info.sebahattindemir@gmail.com>
2023-03-24 22:48:19 +0300

    24/03/2023

--- src/test/java/AppiumClasses/Appium06.java
+++ src/test/java/AppiumClasses/Appium06.java
@@ -22,9 +22,21 @@ public class Appium06 {
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome");
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,"60000");
-        capabilities.setCapability("chromeDriverExecutable","C:\\Users\\sdemi\\IdeaProjects\\mobileTesting\\src\\driver\\chromedriver.exe");
+        capabilities.setCapability("chromedriverExecutable","C:\\Users\\sdemi\\IdeaProjects\\mobileTesting\\src\\driver\\chromedriver.exe");
         AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.get("https://www.amazon.com");
+
+        MobileElement logo = driver.findElementByXPath("//a[@id='nav-logo-sprites']");
+        Assert.assertTrue(logo.isEnabled());
+        System.out.println("Ana sayfadayız");
+        Thread.sleep(7000);
+        MobileElement signInButton = driver.findElementByXPath("//a[@id='nav-logobar-greeting']");
+        signInButton.click();
+        Thread.sleep(7000);
+        MobileElement signInTitle = driver.findElementByXPath("//h2");
+        Assert.assertTrue(signInTitle.isDisplayed());
+
+
 
 
 

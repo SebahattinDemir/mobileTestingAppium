f47ecccf7643b366926649b9d9bfa40ef5dec3c7
Sebahattin Demir <info.sebahattindemir@gmail.com>
2023-03-24 18:32:32 +0300

    24/03/2023

---
+++ src/test/java/AppiumClasses/Appium06.java
@@ -0,0 +1,32 @@
+package AppiumClasses;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium06 {
+
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice"); // Android de fark etmiyor böyle de yazılabilirveya cmd --> adb devices yazdığında gelen değeri de yazabilirsin
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,"60000");
+        capabilities.setCapability("chromeDriverExecutable","C:\\Users\\sdemi\\IdeaProjects\\mobileTesting\\src\\driver\\chromedriver.exe");
+        AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.get("https://www.amazon.com");
+
+
+
+    }
+}

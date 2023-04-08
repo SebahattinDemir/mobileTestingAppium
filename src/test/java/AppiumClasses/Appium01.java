9060df544c4482b22e4b8b23d60c73745035f98f
Sebahattin Demir <info.sebahattindemir@gmail.com>
2023-04-08 21:38:26 +0300

    08/04/2023

--- src/test/java/AppiumClasses/Appium01.java
+++ src/test/java/AppiumClasses/Appium01.java
@@ -28,5 +28,16 @@ public class Appium01 {
 
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
 
+        //capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        //        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        //        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice"); // Android de fark etmiyor böyle de yazılabilirveya cmd --> adb devices yazdığında gelen değeri de yazabilirsin
+        //        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        //        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\sdemi\\IdeaProjects\\mobileTesting\\src\\Apps\\Gesture Tool_1.3_Apkpure.apk");
+        //
+        //        capabilities.setCapability("appPackage","com.davemac327.gesture.tool");
+        //        capabilities.setCapability("appActivity","com.davemac327.gesture.tool.GestureBuilderActivity");
+        //
+        //        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
     }
 }
